d5d28a4eccc3ce4f3833df506913a102ee2e1fb1
JunSeoChoi <evan7484@gmail.com>
2025-07-29 16:14:02 +0900

    Fix: vercel로 배포시 새로고침 문제 해결

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,15 +1,18 @@
 import CTAButton from "../components/CTAButton";
 import { useNavigate } from "react-router-dom";
 import left_chevron from "../assets/chevron/left_chevronImg.svg";
+import Input from "../components/Input";
 
 const Register = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col w-[353px]">
+    <div className="flex flex-col w-[353px] gap-[15px]">
       <div onClick={() => navigate("/login")}>
         <img src={left_chevron} alt="뒤로가기" />
       </div>
+      <Input placeholder="이메일" />
+      <Input placeholder="비밀번호" type="password" />
       <CTAButton onClick={() => navigate("/home")}>회원가입 완료</CTAButton>
     </div>
   );
