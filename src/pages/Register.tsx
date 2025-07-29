5f03365404d9c4d3098317b64d560d76150373ea
JunSeoChoi <evan7484@gmail.com>
2025-07-29 15:14:02 +0900

    Fix: import React 삭제

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import CTAButton from "../components/CTAButton";
 import { useNavigate } from "react-router-dom";
 import left_chevron from "../assets/chevron/left_chevronImg.svg";
--- src/pages/schedule/ScheduleApply.tsx
+++ src/pages/schedule/ScheduleApply.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import left_chevron from "../../assets/chevron/left_chevronImg.svg";
 
