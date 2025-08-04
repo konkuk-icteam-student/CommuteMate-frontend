ab18aa7dad96cd261abfe137a3bc3b73047ebd9f
JunSeoChoi <evan7484@gmail.com>
2025-08-04 13:50:13 +0900

    Design:KRDS 반영

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -12,6 +12,8 @@ const Register = () => {
         <img src={left_chevron} alt="뒤로가기" />
       </div>
       <Input placeholder="이메일" />
+      <Input placeholder="인증번호" />
+      <CTAButton>인증번호 요청</CTAButton>
       <Input placeholder="비밀번호" type="password" />
       <CTAButton onClick={() => navigate("/home")}>회원가입 완료</CTAButton>
     </div>
